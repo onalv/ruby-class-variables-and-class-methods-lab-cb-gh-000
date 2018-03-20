ccf35b1ee053a3c16e85454447e5ac8f314a40eb
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-20 17:47:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
-  
+  @@count
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
 end
